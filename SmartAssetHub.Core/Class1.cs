41bd859025b0371171de05fc086117de72fbf642
Daniel Marujo <danielmarujo2005@gmail.com>
2026-08-13 00:09:48 -0300

    Adicionar arquivos de projeto.

---
+++ SmartAssetHub.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartAssetHub.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SmartAssetHub.Core/Entities/Documento.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace SmartAssetHub.Core.Entities;
+
+[DynamoDBTable("SmartAssetMetadata")]
+public class Documento
+{
+    [DynamoDBHashKey]
+    public string DocumentId { get; set; } = Guid.NewGuid().ToString();
+
+    [DynamoDBProperty]
+    public string NomeArquivo { get; set; } = string.Empty;
+
+    [DynamoDBProperty]
+    public string ChaveS3 { get; set; } = string.Empty;
+
+    [DynamoDBProperty]
+    public string? ResumoIA { get; set; }
+
+    [DynamoDBProperty]
+    public string StatusProcessamento { get; set; } = "PENDENTE";
+
+    [DynamoDBProperty]
+    public DateTime DataUpload { get; set; } = DateTime.UtcNow;
+}
